a471268d8bb558986bb7b4c12e848115f0255d31
akash <godbole1@msu.edu>
2024-10-31 11:30:52 +0000

    bug resolved in serving frontend pages

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from PIL import Image
 import io
@@ -9,10 +9,14 @@ from app import *
 import base64
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 app.register_blueprint(api)
 # CORS(app)  # This will allow CORS for all routes
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
